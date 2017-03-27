5786c3611e4bd32af1909ab9178b2df661bc14f1
Mark Gibson <mark.gibson69@ntlworld.com>
2017-03-27 19:41:36 +0100

    Initial Commit

---
+++ DevslopesGitProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevslopesGitProject
+//
+//  Created by Mark Gibson on 27/03/2017.
+//  Copyright © 2017 MDG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
